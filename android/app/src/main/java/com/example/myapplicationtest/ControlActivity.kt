e85bd4a19440c740ce5b5eeeb33d3d0732669475
Ju Hong Kim <nabimoon1234@gmail.com>
2020-04-02 22:55:33 -0400

    merge attempt fix 2

--- android/app/src/main/java/com/example/myapplicationtest/ControlActivity.kt
+++ android/app/src/main/java/com/example/myapplicationtest/ControlActivity.kt
@@ -1,27 +1,9 @@
 package com.example.myapplicationtest
 
-import android.app.ProgressDialog
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothSocket
-import android.content.Context
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
-import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.control_layout.*
-import org.jetbrains.anko.toast
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
-import java.nio.charset.StandardCharsets.UTF_8
-import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
